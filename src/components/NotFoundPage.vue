480bd7054d2ae3f48e543544019ee9202aadcfdd
ThinkeringAbout <104573860+ThinkeringAbout@users.noreply.github.com>
2022-11-15 23:12:16 +0300

    404 page

--- src/components/NotFoundPage.vue
+++ src/components/NotFoundPage.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="w-full pt-40 flex flex-col justify-center animate-pulse items-center">
-        <h1 class="text-9xl font-mono">404</h1>
-        <h2 class="text-3xl font-mono">Page Not Found</h2>
+    <div class="w-full pt-40 flex flex-col justify-center items-center font-mono">
+        <h1 class="text-9xl">404</h1>
+        <h2 class="text-3xl">Page Not Found</h2>
+        <RouterLink to="/createUser" class="text-2xl underline text-sky-600">Go Home</RouterLink>
     </div>
 </template>
